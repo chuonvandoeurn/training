b79c7512e03556a028f6bb8ae702cde9c497bc0f
chuonvandoeurn <chuonvandoeurn@ymail.com>
2019-09-24 09:58:10 +0700

    See issue #5

---
+++ application/controllers/Issue5.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Issue5 extends CI_Controller {
+
+	public function index()
+	{
+		echo 'this is issue 5';
+	}
+
+}
+
+/* End of file Issue5.php */
+/* Location: ./application/controllers/Issue5.php */
