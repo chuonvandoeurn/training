0e7e908fdf2fe9bfe98b050c4d6470d48d0b62d1
sombath502 <sombath502>
2019-09-24 11:08:55 +0700

    see issue3

---
+++ application/controllers/Issue3.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Issue3 extends CI_Controller {
+
+	public function index()
+	{
+		echo 'Hello';		
+	}
+
+}
+
+/* End of file Issue3.php */
+/* Location: ./application/controllers/Issue3.php */
